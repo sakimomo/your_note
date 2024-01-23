0f0f8624a908948e27eb2d141c7bfaa86ed8b21b
sakimomo <otegamisaki1@icloud.com>
2024-01-23 22:58:37 +0900

    ユーザー管理機能の実装

---
+++ app/models/user.rb
@@ -0,0 +1,12 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  has_many :prototypes
+
+  validates :name, presence: true
+  validates :profile, presence: true
+  validates :password, format: { with: /\A(?=.*[a-zA-Z])(?=.*\d)[a-zA-Z\d]+\z/ }
+end
