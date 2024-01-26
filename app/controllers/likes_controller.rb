00707f0535f62f89f1fbcbf4b6b8274eedd0d564
sakimomo <otegamisaki1@icloud.com>
2024-01-26 23:45:04 +0900

    いいね一覧

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,5 +1,9 @@
 class LikesController < ApplicationController
-  before_action :set_note
+  before_action :authenticate_user!
+
+  def index
+    @likes = current_user.likes.includes(:note)
+  end
 
   def create
     like = current_user.likes.build(note_id: params[:note_id])
@@ -10,14 +14,10 @@ class LikesController < ApplicationController
   end
 
   def destroy
-    like = Like.find_by(note_id: params[:note_id], user_id: current_user.id)
+    like = current_user.likes.find_by(note_id: params[:note_id])
     like.destroy
     respond_to do |format|
       format.js
     end
-  end
-
-  def set_note
-    @note = Note.find(params[:note_id])
   end
 end
