0f4286d8a1cc85937783ab9acc315523b31eb443
sakimomo <otegamisaki1@icloud.com>
2024-01-28 22:12:48 +0900

    フォロー・フォロワー一覧

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   end
   
   resources :users, only: [:show] do
+    member do
+      get :follows, :followers
+    end
     resource :relationships, only: [:create, :destroy]
   end
   
