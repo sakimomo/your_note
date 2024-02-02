88e7a50e85f7ffea34c59a56d074fa0d0aa92b04
sakimomo <otegamisaki1@icloud.com>
2024-02-03 01:07:42 +0900

    投稿機能の単体テストの実施

--- spec/factories/notes.rb
+++ spec/factories/notes.rb
@@ -1,4 +1,12 @@
 FactoryBot.define do
   factory :note do
+    association :user, factory: :user
+    content  { Faker::Lorem.sentence }
+    genre_id { 2 }
+
+    after(:build) do |note|
+      note.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
+      note.save
+    end
   end
 end
